2cbc680598071e34212accae2ea4e734b15837ee
Tiaohsun <Tiaohsun@hotmail.com>
2022-11-02 18:09:35 +0800

    初始化 WebApi&ClinetApp 專案

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<script>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+//import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+
+export default {
+ data() {
+   return {
+     post: null,
+   };
+ },
+ created() {
+   // fetch the data when the view is created and the data is
+   // already being observed
+   this.fetchData();
+ },
+ methods: {
+   fetchData() {
+    // axios.get('/api/Activity', {
+    //       params: {
+    //           urlPath: 'focusline'
+    //       }
+    //   }).then(({ data }) => {
+    //       this.post = data;
+    //   });
+      axios.get('/weatherforecast').then(({data}) => {
+        this.post = data;
+      })
+
+    // fetch('/weatherforecast')
+    //                 .then(r => {
+    //                   console.log(r); 
+    //                   return r.json();
+    //                 })
+    //                 .then(json => {
+    //                     this.post = json;
+
+    //                     return;
+    //                 });
+   },
+ },
+};
+</script>
+
+<template>
+    <div>
+        <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://vuejs.org/" target="_blank">
+            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+        </a>
+    </div>
+    <!-- <HelloWorld msg="Vite + Vue" /> -->
+</template>
+
+<style scoped>
+    .logo {
+        height: 6em;
+        padding: 1.5em;
+        will-change: filter;
+    }
+
+        .logo:hover {
+            filter: drop-shadow(0 0 2em #646cffaa);
+        }
+
+        .logo.vue:hover {
+            filter: drop-shadow(0 0 2em #42b883aa);
+        }
+</style>
---
+++ vite.config.js
@@ -0,0 +1,46 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { readFileSync } from 'fs'
+import { join } from 'path'
+
+const baseFolder =
+    process.env.APPDATA !== undefined && process.env.APPDATA !== ''
+        ? `${process.env.APPDATA}/ASP.NET/https`
+        : `${process.env.HOME}/.aspnet/https`;
+
+const certificateArg = process.argv.map(arg => arg.match(/--name=(?<value>.+)/i)).filter(Boolean)[0];
+const certificateName = certificateArg ? certificateArg.groups.value : "RaceEntry.ClientApp";
+
+if (!certificateName) {
+    console.error('Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly.')
+    process.exit(-1);
+}
+
+const certFilePath = join(baseFolder, `${certificateName}.pem`);
+const keyFilePath = join(baseFolder, `${certificateName}.key`);
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [vue()],
+    server:{
+        https: {
+            key: readFileSync(keyFilePath),
+            cert: readFileSync(certFilePath),
+        },
+        host: true,
+        port: 5002,
+        proxy: {
+            '^/weatherforecast': {
+                target: 'https://localhost:7256',
+                changeOrigin: true,
+                secure: false,
+            },
+            // '/api': {
+            //     target: 'https://my.focusline.com.tw',
+            //     changeOrigin: true,
+            //     secure: false,
+            // },
+        }
+    }
+
+})
