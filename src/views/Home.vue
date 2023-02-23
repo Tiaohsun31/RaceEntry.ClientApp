3a7cf917eb69b90f6f1a8fef383a3dae30a0079a
Tiaohsun <Tiaohsun@hotmail.com>
2023-02-23 10:08:22 +0800

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,7 @@
                             </div>
                         </template>
                     </div>
-                </div>
+                </div>1
                 <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="sr-only">Previous</span>
