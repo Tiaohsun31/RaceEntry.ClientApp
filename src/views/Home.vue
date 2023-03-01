d6ec098f35b995847eeda2da7a909b881141791e
wangch15 <113665679+wangch15@users.noreply.github.com>
2023-03-01 17:44:11 +0800

    刪除舊有code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -48,46 +48,6 @@
 
     <!-- Banner -->
     <SwiperBanner :bannerArray="banners" />
-    <!-- <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
-            <div class="carousel-inner">
-                <template v-for="(item,index) in banners">
-                    <div class="carousel-item" v-bind:class="{ active:index == 0 }">
-                        <div class="card border-0 mb-3">
-                            <div class="row bgc-light">
-                                <div class="col-md-4 align-self-center d-none d-md-block">
-                                    <div class="card-body text-center text-secondary ">
-                                        <h2 class="card-title text-primary font-bolder" v-text="item.actName"> </h2>
-                                        <p class="card-text h3 mt-4" v-text="'活動時間:' + shortDate(item.actDate)"> </p>
-                                        <p class="card-text h3 mt-4" v-text="'報名截止:' + shortDate(item.regETime)"> </p>
-                                        <p class="card-text h3 mt-4" v-text="item.address"> </p>
-                                        <RouterLink v-if="item.canSignUp" :to="`/${item.actCode}#signup`" 
-                                            class="btn btn-lg btn-orange rounded-pill mt-5 px-5">
-                                            我要報名
-                                        </RouterLink>
-                                        <router-link v-bind:to="{name:'HomePage',params:{code:item.actCode}}"
-                                        class="btn btn-lg btn-info rounded-pill mt-5 px-5">
-                                            活動詳情
-                                        </router-link>
-                                    </div>
-                                </div>
-                                <div class="col-md-8 col-12">
-                                    <img v-bind:src="item.rectangle" class="img-fluid rounded-start d-none d-sm-block" v-bind:alt="item.actName">
-                                    <img v-bind:src="item.square" class="img-fluid rounded-start d-block d-sm-none" v-bind:alt="item.actName">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </template>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div> -->
     <!-- End Banner-->
 
     <!-- Search and Filter-->
@@ -101,88 +61,6 @@
     <section class="moreActArea">
       <router-link to="/acts" >更多活動</router-link>
     </section>
-
-    <!-- <div class="border-t-4 brc-orange mt-5 pb-5 bgc-primary-l3">
-      <div class="my-5 container">
-        <div class="row">
-          <template v-for="item in recommendActs">
-            <div class="col-md-3 col-12 mb-4">
-              <div
-                class="card pos-rel shadow-sm"
-                v-bind:class="
-                  item.displayLabel == '推薦賽事' ? 'border-3 brc-orange' : ''
-                "
-              >
-                <span
-                  v-show="item.displayLabel == '推薦賽事'"
-                  class="badge bgc-orange-d2 text-white position-tl m-n25 badge-pill text-110 border-3 brc-white-tp6 radius-4"
-                  style="z-index: 1"
-                  >推薦賽事</span
-                >
-                <div class="d-style overflow-hidden">
-                  <img
-                    v-bind:src="item.square"
-                    class="img-fluid d-zoom-2"
-                    v-bind:alt="item.name"
-                    style="height: 250px; z-index: 0"
-                  />
-                </div>
-                <div class="card-body">
-                  <div class="card-title text-primary h5 text-truncate">
-                    {{ item.actName }}
-                  </div>
-                  <div class="card-text text-secondary mt-2">
-                    活動日期: {{ actDate(item.actDate) }}
-                  </div>
-                  <div class="card-text text-secondary mt-2">
-                    報名截止: {{ shortDate(item.shortDate) }}
-                  </div>
-                  <div class="card-text text-secondary mt-2">
-                    {{ item.address }}
-                  </div>
-                  <div class="card-text mt-2">
-                    <span
-                      class="badge bgc-secondary-l3 text-secondary-d3 text-80 px-2 mr-1"
-                      v-for="tag in item.kmTags?.split(',')"
-                    >
-                      {{ tag }}
-                    </span>
-                  </div>
-                  <div class="row mt-4">
-                    <div class="col">
-                      <router-link
-                        v-bind:to="{
-                          name: 'HomePage',
-                          params: { code: item.actCode },
-                        }"
-                        class="btn btn-block btn-info text-white rounded-pill px-4"
-                      >
-                        活動詳情
-                      </router-link>
-                    </div>
-
-                    <div v-if="item.canSignUp" class="col">
-                      <router-link
-                        v-bind:to="`/${item.actCode}#signup`"
-                        class="btn btn-block btn-orange rounded-pill px-4"
-                      >
-                        我要報名
-                      </router-link>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </template>
-        </div>
-        <div class="text-center mt-5">
-          <router-link to="/acts" class="btn btn-lg btn-orange px-5">
-            更多賽事
-          </router-link>
-        </div>
-      </div>
-    </div> -->
-
     <!-- End Act List -->
   </Layout>
 </template>
